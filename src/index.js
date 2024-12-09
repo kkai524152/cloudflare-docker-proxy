0da7b0a6fe866a9d721216b167c7fc448f27d15f
kkai524152 <97440194+kkai524152@users.noreply.github.com>
2024-12-09 17:26:46 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  ["docker." + CUSTOM_DOMAIN]: dockerHub,
-  ["quay." + CUSTOM_DOMAIN]: "https://quay.io",
-  ["gcr." + CUSTOM_DOMAIN]: "https://gcr.io",
-  ["k8s-gcr." + CUSTOM_DOMAIN]: "https://k8s.gcr.io",
-  ["k8s." + CUSTOM_DOMAIN]: "https://registry.k8s.io",
-  ["ghcr." + CUSTOM_DOMAIN]: "https://ghcr.io",
-  ["cloudsmith." + CUSTOM_DOMAIN]: "https://docker.cloudsmith.io",
-  ["ecr." + CUSTOM_DOMAIN]: "https://public.ecr.aws",
+  ["docker." + 001211.xyz]: dockerHub,
+  ["quay." + 001211.xyz]: "https://quay.io",
+  ["gcr." + 001211.xyz]: "https://gcr.io",
+  ["k8s-gcr." + 001211.xyz]: "https://k8s.gcr.io",
+  ["k8s." + 001211.xyz]: "https://registry.k8s.io",
+  ["ghcr." + 001211.xyz]: "https://ghcr.io",
+  ["cloudsmith." + 001211.xyz]: "https://docker.cloudsmith.io",
+  ["ecr." + 001211.xyz]: "https://public.ecr.aws",
 
   // staging
   ["docker-staging." + CUSTOM_DOMAIN]: dockerHub,
